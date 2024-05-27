53669804f5ddca13036abc8183c5167dca5839ea
Volodymyr Korzhevskyi <korzhevskyiv@gmail.com>
2024-05-27 19:08:51 +0200

    change theme switcher && add logics for  him

--- src/layouts/dashboard/index.js
+++ src/layouts/dashboard/index.js
@@ -1,17 +1,63 @@
-import { useTheme } from "@mui/material/styles";
+import { useTheme, styled } from "@mui/material/styles";
 import { Box, Stack, IconButton, Divider, Avatar, Switch } from "@mui/material";
 import { React, useState } from "react";
 import { Outlet } from "react-router-dom";
 import { Gear } from "phosphor-react";
 import { faker } from "@faker-js/faker";
+import useSettings from "../../hooks/useSettings";
 
 import Logo from "../../assets/Images/logo.ico";
 import { Nav_Buttons } from "../../data";
+
+const AntSwitch = styled(Switch)(({ theme }) => ({
+  width: 40,
+  height: 20,
+  padding: 0,
+  display: "flex",
+  "&:active": {
+    "& .MuiSwitch-thumb": {
+      width: 15,
+    },
+    "& .MuiSwitch-switchBase.Mui-checked": {
+      transform: "translateX(9px)",
+    },
+  },
+  "& .MuiSwitch-switchBase": {
+    padding: 2,
+    "&.Mui-checked": {
+      transform: "translateX(20px)",
+      color: "#fff",
+      "& + .MuiSwitch-track": {
+        opacity: 1,
+        backgroundColor: theme.palette.mode === "dark" ? "#177ddc" : "#1890ff",
+      },
+    },
+  },
+  "& .MuiSwitch-thumb": {
+    boxShadow: "0 2px 4px 0 rgb(0 35 11 / 20%)",
+    width: 16,
+    height: 16,
+    borderRadius: 8,
+    transition: theme.transitions.create(["width"], {
+      duration: 200,
+    }),
+  },
+  "& .MuiSwitch-track": {
+    borderRadius: 20 / 2,
+    opacity: 1,
+    backgroundColor:
+      theme.palette.mode === "dark"
+        ? "rgba(255,255,255,.35)"
+        : "rgba(0,0,0,.25)",
+    boxSizing: "border-box",
+  },
+}));
 
 const DashboardLayout = () => {
   const theme = useTheme();
 
   const [selected, setSelected] = useState(0);
+  const { onToggleMode } = useSettings();
 
   console.log(theme);
 
@@ -103,7 +149,11 @@ const DashboardLayout = () => {
           </Stack>
           <Stack alignItems={"center"} spacing={4}>
             {/* Here will be switch, in the future... */}
-            <Switch></Switch>
+            <AntSwitch
+              onChange={() => {
+                onToggleMode();
+              }}
+            ></AntSwitch>
             <Avatar src={faker.image.avatar()}></Avatar>
           </Stack>
         </Stack>
