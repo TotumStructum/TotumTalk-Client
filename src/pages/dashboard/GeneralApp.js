cb576d675a154c2a37d26f0d4a7dc37f6978009e
Volodymyr Korzhevskyi <korzhevskyiv@gmail.com>
2024-06-09 23:21:41 +0200

    feat(Conversation/index,js): make header for single-chat component

--- src/pages/dashboard/GeneralApp.js
+++ src/pages/dashboard/GeneralApp.js
@@ -1,12 +1,23 @@
+import { Stack, Box } from "@mui/material";
 import React from "react";
 
+import Conversation from "../../components/Conversation";
 import Chats from "./Chats";
 
 const GeneralApp = () => {
   return (
-    <>
+    <Stack direction={"row"} sx={{ width: "100%" }}>
       <Chats />
-    </>
+      <Box
+        sx={{
+          height: "100%",
+          width: "calc(100vw - 420px)",
+          backgroundColor: "#fff",
+        }}
+      >
+        <Conversation />
+      </Box>
+    </Stack>
   );
 };
 
