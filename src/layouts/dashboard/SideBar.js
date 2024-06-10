c2a1cbbc34fbeaf5bee148265473f5862ae12fb7
Volodymyr Korzhevskyi <korzhevskyiv@gmail.com>
2024-06-10 14:16:05 +0200

    refactor(dashboard/index.js): move the sidebar to a separate component

---
+++ src/layouts/dashboard/SideBar.js
@@ -0,0 +1,133 @@
+import {
+  Avatar,
+  Box,
+  Divider,
+  IconButton,
+  Stack,
+  useTheme,
+} from "@mui/material";
+import React, { useState } from "react";
+import { faker } from "@faker-js/faker";
+import { Gear } from "phosphor-react";
+
+import AntSwitch from "../../components/AntSwitch";
+import useSettings from "../../hooks/useSettings";
+import Logo from "../../assets/Images/logo.ico";
+import { Nav_Buttons } from "../../data";
+
+const SideBar = () => {
+  const theme = useTheme();
+  const [selected, setSelected] = useState(0);
+  const { onToggleMode } = useSettings();
+
+  return (
+    <Box
+      p={2}
+      sx={{
+        backgroundColor: theme.palette.background.paper,
+        boxShadow: "0px 0px 2px rgba(0, 0, 0, 0.25)",
+        height: "100vh",
+        width: 100,
+      }}
+    >
+      <Stack
+        direction={"column"}
+        alignItems={"center"}
+        justifyContent="space-between"
+        sx={{ height: "100%" }}
+        spacing={3}
+      >
+        <Stack alignItems="center" spacing={4}>
+          <Box
+            sx={{
+              backgroundColor: theme.palette.primary.main,
+              height: 64,
+              width: 64,
+              borderRadius: 1.5,
+            }}
+          >
+            <img src={Logo} alt={"Logo"} />
+          </Box>
+          <Stack
+            sc={{ width: "max-content" }}
+            direction="column"
+            alignItems="center"
+            spacing={3}
+          >
+            {Nav_Buttons.map((el) =>
+              el.index === selected ? (
+                <Box
+                  sx={{
+                    backgroundColor: theme.palette.primary.main,
+                    borderRadius: 1.5,
+                  }}
+                >
+                  <IconButton
+                    sx={{ width: "max-content", color: "#fff" }}
+                    key={el.index}
+                  >
+                    {el.icon}
+                  </IconButton>
+                </Box>
+              ) : (
+                <IconButton
+                  onClick={() => {
+                    setSelected(el.index);
+                  }}
+                  sx={{
+                    width: "max-content",
+                    color:
+                      theme.palette.mode === "light"
+                        ? "#000"
+                        : theme.palette.text.primary,
+                  }}
+                  key={el.index}
+                >
+                  {el.icon}
+                </IconButton>
+              )
+            )}
+            <Divider sx={{ width: "48px" }} />
+            {selected === 3 ? (
+              <Box
+                sx={{
+                  backgroundColor: theme.palette.primary.main,
+                  borderRadius: 1.5,
+                }}
+              >
+                <IconButton sx={{ width: "max-content", color: "#fff" }}>
+                  <Gear />
+                </IconButton>
+              </Box>
+            ) : (
+              <IconButton
+                onClick={() => {
+                  setSelected(3);
+                }}
+                sx={{
+                  width: "max-content",
+                  color:
+                    theme.palette.mode === "light"
+                      ? "#000"
+                      : theme.palette.text.primary,
+                }}
+              >
+                <Gear />
+              </IconButton>
+            )}
+          </Stack>
+        </Stack>
+        <Stack alignItems={"center"} spacing={4}>
+          {/* Here will be switch, in the future... */}
+          <AntSwitch
+            defaultChecked={theme.palette.mode === "dark"}
+            onChange={onToggleMode}
+          />
+          <Avatar src={faker.image.avatar()}></Avatar>
+        </Stack>
+      </Stack>
+    </Box>
+  );
+};
+
+export default SideBar;
