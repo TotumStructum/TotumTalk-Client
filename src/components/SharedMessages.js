805bf0b95d62e751ebdd8903e7e5f2a5eaab6f1c
Volodymyr Korzhevskyi <korzhevskyiv@gmail.com>
2024-11-22 11:03:46 +0100

    feat(SharedMessages.js)make a shared messages panel

--- src/components/SharedMessages.js
+++ src/components/SharedMessages.js
@@ -1,20 +1,87 @@
-import { Box, IconButton } from "@mui/material";
-import { ArrowLeft } from "phosphor-react";
+import { Box, IconButton, Stack, Tab, Tabs, Typography } from "@mui/material";
 import React from "react";
+import { CaretLeft } from "phosphor-react";
 import { useDispatch } from "react-redux";
 import { UpdateSidebarType } from "../redux/slices/app";
+import { useTheme } from "@emotion/react";
 
 const SharedMessages = () => {
+  const theme = useTheme();
+
+  const [value, setValue] = React.useState(0);
+
+  const handleChange = (event, newValue) => {
+    setValue(newValue);
+  };
+
   const dispatch = useDispatch();
   return (
     <Box sx={{ width: 320, height: "100vh" }}>
-      <IconButton
-        onClick={() => {
-          dispatch(UpdateSidebarType("CONTACT"));
-        }}
-      >
-        <ArrowLeft />
-      </IconButton>
+      <Stack sx={{ height: "100%" }}>
+        <Box
+          sx={{
+            boxShadow: "0px 0px 2px rgba(0,0,0,0.25)",
+            width: "100%",
+            backgroundColor:
+              theme.palette.mode === "light"
+                ? "#F8FAFF"
+                : theme.palette.background,
+          }}
+        >
+          <Stack
+            sx={{ height: "100%", p: 2 }}
+            direction="row"
+            alignItems={"center"}
+            spacing={3}
+          >
+            <IconButton
+              onClick={() => {
+                dispatch(UpdateSidebarType("CONTACT"));
+              }}
+            >
+              <CaretLeft />
+            </IconButton>
+            <Typography variant="subtitle2">Shared Messages</Typography>
+          </Stack>
+        </Box>
+        <Tabs
+          sx={{ px: 2, pt: 2 }}
+          value={value}
+          onChange={handleChange}
+          centered
+        >
+          <Tab label="Media" />
+          <Tab label="Links" />
+          <Tab label="Docs" />
+        </Tabs>
+        <Stack
+          sx={{
+            height: "100%",
+            position: "relative",
+            flexGrow: 1,
+            overflowY: "scroll",
+          }}
+          p={3}
+          spacing={3}
+        >
+          {(() => {
+            switch (value) {
+              case 0:
+                //Media
+                break;
+              case 1:
+                //Links
+                break;
+              case 2:
+                //Docs
+                break;
+
+              default:
+                break;
+            }
+          })()}
+        </Stack>
+      </Stack>
     </Box>
   );
 };
