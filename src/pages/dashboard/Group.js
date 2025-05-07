cb4ccedc9dc8bf6fdec7f6d77bc14744ac17c819
Volodymyr Korzhevskyi <korzhevskyiv@gmail.com>
2025-05-07 13:48:03 +0200

    feat(Group.js): start making a left side of the groups chat dashboard

--- src/pages/dashboard/Group.js
+++ src/pages/dashboard/Group.js
@@ -1,7 +1,77 @@
+import {
+  Box,
+  Stack,
+  Typography,
+  styled,
+  InputBase,
+  alpha,
+} from "@mui/material";
 import React from "react";
+import { MagnifyingGlass } from "phosphor-react";
+
+const Search = styled("div")(({ theme }) => ({
+  position: "relative",
+  borderRadius: 20,
+  backgroundColor: alpha(theme.palette.background.default, 1),
+  marginRight: theme.spacing(2),
+  marginLeft: 0,
+  width: "100%",
+}));
+
+const SearchIconWrapper = styled("div")(({ theme }) => ({
+  padding: theme.spacing(0, 2),
+  height: "100%",
+  position: "absolute",
+  pointerEvents: "none",
+  display: "flex",
+  alignItems: "center",
+  justifyContent: "center",
+}));
+
+const SearchInputBase = styled(InputBase)(({ theme }) => ({
+  color: "inherit",
+  "& .MuiInputBase-input": {
+    padding: theme.spacing(1, 1, 1, 0),
+    paddingLeft: `calc(1em + ${theme.spacing(4)})`,
+    width: "100%",
+  },
+}));
 
 const Group = () => {
-  return <div>Group</div>;
+  return (
+    <>
+      <Stack direction={"row"} sx={{ width: "100%" }}>
+        {/* {left} */}
+        <Box
+          sx={{
+            height: "100vh",
+            backgroundColor: (theme) =>
+              theme.palette.mode === "light"
+                ? "#F8FAFF"
+                : theme.palette.background,
+            width: 320,
+            boxShadow: "0px 0px 2px rgba(0, 0, 0, 0.2)",
+          }}
+        >
+          <Stack p={3} spacing={2} sx={{ maxHeight: "100vh" }}>
+            <Stack>
+              <Typography variant="h5">Groups</Typography>
+            </Stack>
+
+            <Stack sx={{ width: "100%" }}>
+              <Search>
+                <SearchIconWrapper>
+                  <MagnifyingGlass color="#709ce6" />
+                </SearchIconWrapper>
+                <SearchInputBase placeholder="Search..." />
+              </Search>
+            </Stack>
+          </Stack>
+        </Box>
+        {/* {right} */}
+      </Stack>
+    </>
+  );
 };
 
 export default Group;
