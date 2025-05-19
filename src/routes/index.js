ad6be35babee222eb09c43f85ef5351e7ddba5e0
Volodymyr Korzhevskyi <korzhevskyiv@gmail.com>
2025-05-19 15:28:12 +0200

    feat(Call.js, routes/index.js): make a page for call && add route for call page

--- src/routes/index.js
+++ src/routes/index.js
@@ -49,6 +49,7 @@ export default function Router() {
         { path: "app", element: <GeneralApp /> },
         { path: "settings", element: <Settings /> },
         { path: "group", element: <GroupPage /> },
+        { path: "call", element: <CallPage /> },
 
         { path: "404", element: <Page404 /> },
         { path: "*", element: <Navigate to="/404" replace /> },
@@ -70,6 +71,8 @@ const ResetPasswordPage = Loadable(
 const NewPasswordPage = Loadable(
   lazy(() => import("../pages/auth/NewPassword"))
 );
+
+const CallPage = Loadable(lazy(() => import("../pages/dashboard/Call")));
 
 const GroupPage = Loadable(lazy(() => import("../pages/dashboard/Group")));
 
